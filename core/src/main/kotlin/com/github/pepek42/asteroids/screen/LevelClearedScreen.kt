403ea90659a2c4c4a15506a53308d6db6a4f4d00
Pepek42 <8071251+pepek42@users.noreply.github.com>
2023-11-08 16:25:42 +0100

    Level cleared screen

--- core/src/main/kotlin/com/github/pepek42/asteroids/screen/LevelClearedScreen.kt
+++ core/src/main/kotlin/com/github/pepek42/asteroids/screen/LevelClearedScreen.kt
@@ -1,16 +1,74 @@
 package com.github.pepek42.asteroids.screen
 
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.I18NBundle
 import com.github.pepek42.asteroids.Game
+import com.github.pepek42.asteroids.GameState
+import com.github.pepek42.asteroids.event.GameEventManager
+import ktx.actors.onClick
+import ktx.actors.plusAssign
 import ktx.app.KtxScreen
+import ktx.app.clearScreen
+import ktx.log.logger
+import ktx.scene2d.label
+import ktx.scene2d.scene2d
+import ktx.scene2d.textButton
 
 class LevelClearedScreen(
-    private val game: Game,
+    game: Game,
 ) : KtxScreen {
+    private val i18NBundle = game.get<I18NBundle>()
+    private val gameState: GameState = game.get<GameState>()
+    private val table: Table = Table()
+    private val centerLabel: Label = scene2d.label("").apply { isVisible = false }
+    private val stage = game.get<Stage>()
+    private val nextLevelButton = scene2d.textButton(i18NBundle["next_level_button"]) {
+        onClick { game.setScreen<PlayScreen>() }
+    }
+    private val gameEventManager = game.get<GameEventManager>()
+
+    init {
+        table.setFillParent(true)
+        table.defaults().pad(3f).expand().fill()
+        table.add()
+        table.add()
+        table.add()
+        table.row()
+        table.add()
+        table.add(centerLabel).align(Align.center)
+        table.add()
+        table.row()
+        table.add()
+        table.add()
+        table.add(nextLevelButton)
+    }
+
     override fun show() {
-        // TODO
+        centerLabel.isVisible = false
+        centerLabel.setText(i18NBundle.format("level_cleared_message", gameState.level))
+        gameEventManager.ignorePlayerInputs()
+        stage += table
+    }
+
+    override fun resize(width: Int, height: Int) {
+        logger.info { "Resizing screen to ${width}x$height" }
+        stage.viewport.update(width, height, true)
+    }
+
+    override fun render(delta: Float) {
+        clearScreen(red = 0f, green = 0f, blue = 0f)
+        stage.act()
+        stage.draw()
     }
 
     override fun hide() {
-        // TODO
+        table.remove()
+    }
+
+    companion object {
+        private val logger = logger<LevelClearedScreen>()
     }
 }
